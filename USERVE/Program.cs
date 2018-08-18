22da2a640f91223a5b066633a237ca2683fcc007
Dimitrius Floyd <dimitriusfloyd@gmail.com>
2018-08-18 18:16:43 -0500

    first commit

--- USERVE/Program.cs
+++ USERVE/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace USERVE
 {
@@ -8,5 +12,35 @@ namespace USERVE
         {
             Console.WriteLine("Hello World!");
         }
+
+        static void AddNewCategory(string newCategory)
+        {
+
+            IConfiguration configBuilder = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+#if DEBUG
+                .AddJsonFile("appsettings.debug.json")
+#else
+                .AddJsonFile("appsettings.release.json")
+#endif
+                .Build();
+
+            string connStr = configBuilder.GetConnectionString("DefaultConnection");
+
+            MySqlConnection conn = new MySqlConnection(connStr);
+
+            using (conn)
+            {
+                conn.Open();
+
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "INSERT INTO categories (Name) VALUES (@newCategory)";
+                cmd.Parameters.AddWithValue("newCategory", newCategory);
+
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+
     }
 }
