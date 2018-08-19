c71bf095aeac2867a735ff004f9990b0c4c1a2d5
Dimitrius Floyd <dimitriusfloyd@gmail.com>
2018-08-19 11:00:14 -0500

    10

--- USERVE/Program.cs
+++ USERVE/Program.cs
@@ -10,37 +10,14 @@ namespace USERVE
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
         }
+        
+      
 
-        static void AddNewCategory(string newCategory)
-        {
+        
 
-            IConfiguration configBuilder = new ConfigurationBuilder()
-               .SetBasePath(Directory.GetCurrentDirectory())
-#if DEBUG
-                .AddJsonFile("appsettings.debug.json")
-#else
-                .AddJsonFile("appsettings.release.json")
-#endif
-                .Build();
 
-            string connStr = configBuilder.GetConnectionString("DefaultConnection");
-
-            MySqlConnection conn = new MySqlConnection(connStr);
-
-            using (conn)
-            {
-                conn.Open();
-
-                MySqlCommand cmd = conn.CreateCommand();
-                cmd.CommandText = "INSERT INTO categories (Name) VALUES (@newCategory)";
-                cmd.Parameters.AddWithValue("newCategory", newCategory);
-
-                cmd.ExecuteNonQuery();
-            }
-
-        }
 
     }
 }
---
+++ USERVE/methods.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace USERVE
+{
+    class Methods
+    {
+        static void NewRestaurant(string Name, string Address)
+        {
+
+            IConfiguration configBuilder = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+#if DEBUG
+                .AddJsonFile("appsettings.debug.json")
+#else
+                .AddJsonFile("appsettings.release.json")
+#endif
+                .Build();
+
+            string connStr = configBuilder.GetConnectionString("DefaultConnection");
+
+            MySqlConnection conn = new MySqlConnection(connStr);
+
+            using (conn)
+            {
+                conn.Open();
+
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "INSERT INTO restaurants (Name, Address) VALUES (@Name, @Address);";
+                cmd.Parameters.AddWithValue("Name", Name);
+                cmd.Parameters.AddWithValue("Address", Address);
+
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+
+        static void AddNewShelter(string Name, string Address, int Capacity)
+        {
+
+            IConfiguration configBuilder = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+#if DEBUG
+                .AddJsonFile("appsettings.debug.json")
+#else
+                .AddJsonFile("appsettings.release.json")
+#endif
+                .Build();
+
+            string connStr = configBuilder.GetConnectionString("DefaultConnection");
+
+            MySqlConnection conn = new MySqlConnection(connStr);
+
+            using (conn)
+            {
+                conn.Open();
+
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "INSERT INTO shelters (Name, Address, Capacity) VALUES (@Name, @Address, @Capacity);";
+                cmd.Parameters.AddWithValue("Name", Name);
+                cmd.Parameters.AddWithValue("Address", Address);
+
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+
+        static void AddNewTrasporter(string name, string vehicle, int driversliscense)
+        {
+
+            IConfiguration configBuilder = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+#if DEBUG
+                .AddJsonFile("appsettings.debug.json")
+#else
+                .AddJsonFile("appsettings.release.json")
+#endif
+                .Build();
+
+            string connStr = configBuilder.GetConnectionString("DefaultConnection");
+
+            MySqlConnection conn = new MySqlConnection(connStr);
+
+            using (conn)
+            {
+                conn.Open();
+
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "INSERT INTO categories (name, vehicle, driversliscense) VALUES (@name, @vehicle, @driversliscense);";
+                cmd.Parameters.AddWithValue("name", name);
+                cmd.Parameters.AddWithValue("vehicle", vehicle);
+                cmd.Parameters.AddWithValue("driversliscense", driversliscense);
+
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+    }
+}
